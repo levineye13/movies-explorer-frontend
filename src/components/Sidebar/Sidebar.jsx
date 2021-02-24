aba131c8a074b52c2e7cf771e29168a23f34c3f8
Oleg Zharov <levineye@gmail.com>
2021-02-24 16:35:46 +0300

    feat: сверстать сайдбар

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
+import './Sidebar.css';
+
+const Sidebar = ({ isOpen, onClose }) => {
+  const handleCloseMenuScreenClick = ({ target }) => {
+    if (target.classList.contains('sidebar_opened')) {
+      onClose();
+    }
+  };
+
+  return (
+    <aside
+      className={`sidebar ${isOpen ? 'sidebar_opened' : ''}`}
+      onClick={handleCloseMenuScreenClick}
+    >
+      <div
+        className={`sidebar__content ${
+          isOpen ? 'sidebar__content_opened' : ''
+        }`}
+      >
+        <button className="sidebar__button" type="button" onClick={onClose} />
+        <nav className="sidebar__menu">
+          <ul className="sidebar__list">
+            <li className="sidebar__item">
+              <NavLink
+                exact
+                to="/"
+                className="sidebar__link"
+                activeClassName="sidebar__link_active"
+              >
+                Главная
+              </NavLink>
+            </li>
+            <li className="sidebar__item">
+              <NavLink
+                to="/movies"
+                className="sidebar__link"
+                activeClassName="sidebar__link_active"
+              >
+                Фильмы
+              </NavLink>
+            </li>
+            <li className="sidebar__item">
+              <NavLink
+                to="/saved-movies"
+                className="sidebar__link"
+                activeClassName="sidebar__link_active"
+              >
+                Сохранённые фильмы
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+        <Link to="/profile" className="sidebar__account">
+          Аккаунт
+        </Link>
+      </div>
+    </aside>
+  );
+};
+
+export default Sidebar;
