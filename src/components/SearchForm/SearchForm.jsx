9481252c81a4b2d995bb8d9f56e785334695ea62
Oleg Zharov <levineye@gmail.com>
2021-02-27 23:27:01 +0300

    feat: добавить компонент формы поиска фильмов

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
-import InputElement from './../InputElement/InputElement';
-import searchImg from './../../images/movies-search.svg';
+import FilterCheckbox from './../FilterCheckbox/FilterCheckbox';
 import './SearchForm.css';
 
 const SearchForm = () => {
   return (
-    <form action="#" className="search-form" name="searchForm" noValidate>
+    <form
+      action="#"
+      className="search-form movies__search-form"
+      name="searchForm"
+      noValidate
+    >
       <label className="search-form__label">
         <span className="search-form__search-img" />
-        <InputElement type="text" placeholder="Фильм" />
+        <input
+          type="text"
+          className="search-form__input"
+          name="movieInput"
+          placeholder="Фильм"
+          required
+        />
         <button className="search-form__submit" type="submit" />
       </label>
+      <FilterCheckbox />
     </form>
   );
 };
