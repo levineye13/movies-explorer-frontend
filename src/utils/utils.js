7589025bfdd8f9b11007cdda24acc7da9947efc7
Oleg Zharov <levineye@gmail.com>
2021-02-27 23:16:45 +0300

    feat: добавить картинки статически, пока не обращаемся к серверу

---
+++ src/utils/utils.js
@@ -0,0 +1,27 @@
+import img1 from './../images/movies-cards/pic__COLOR_pic1.jpg';
+import img2 from './../images/movies-cards/pic__COLOR_pic2.jpg';
+import img3 from './../images/movies-cards/pic__COLOR_pic3.jpg';
+import img4 from './../images/movies-cards/pic__COLOR_pic4.jpg';
+import img5 from './../images/movies-cards/pic__COLOR_pic5.jpg';
+import img6 from './../images/movies-cards/pic__COLOR_pic6.jpg';
+import img7 from './../images/movies-cards/pic__COLOR_pic7.jpg';
+import img8 from './../images/movies-cards/pic__COLOR_pic8.jpg';
+import img9 from './../images/movies-cards/pic__COLOR_pic9.jpg';
+import img10 from './../images/movies-cards/pic__COLOR_pic10.jpg';
+import img11 from './../images/movies-cards/pic__COLOR_pic11.jpg';
+import img12 from './../images/movies-cards/pic__COLOR_pic12.jpg';
+
+export const images = [
+  img1,
+  img2,
+  img3,
+  img4,
+  img5,
+  img6,
+  img7,
+  img8,
+  img9,
+  img10,
+  img11,
+  img12,
+];
