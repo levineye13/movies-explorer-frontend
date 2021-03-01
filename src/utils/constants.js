eaafe67078dc1fe9674426e4cef485141a90dc05
Oleg Zharov <levineye@gmail.com>
2021-03-01 22:12:05 +0300

    feat: вынести константы в отдельны файл

---
+++ src/utils/constants.js
@@ -0,0 +1,24 @@
+const root = '/';
+const movies = '/movies';
+const saved = '/saved-movies';
+const profile = '/profile';
+const signin = '/signin';
+const signup = '/signup';
+
+export const PATHNAME = {
+  root,
+  movies,
+  saved,
+  profile,
+  signin,
+  signup,
+};
+
+export const HEADER_TYPE = {
+  landing: 'landing',
+  dark: 'dark',
+};
+
+export const HEADER_DISPLAY = [root, movies, saved, profile];
+
+export const FOOTER_DISPLAY = [root, movies, saved];
