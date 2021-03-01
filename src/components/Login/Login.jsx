236951628a7d2afd2a9c895ba4bb80efe813fa4a
Oleg Zharov <levineye@gmail.com>
2021-03-01 22:11:16 +0300

    fix: добавить секцию логина

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,34 @@
+import React, { useRef } from 'react';
+import './Login.css';
+import AuthenticationSection from '../AuthenticationSection/AuthenticationSection';
+import InputElement from '../InputElement/InputElement';
+
+const Login = () => {
+  const firstInputRef = useRef();
+
+  const loginChildren = (
+    <>
+      <InputElement
+        inputTitle="E-mail"
+        type="email"
+        name="emailInput"
+        inputRef={firstInputRef}
+      />
+      <InputElement inputTitle="Пароль" type="password" name="passwordInput" />
+    </>
+  );
+
+  return (
+    <AuthenticationSection
+      title="Рады видеть!"
+      children={loginChildren}
+      textButton="Войти"
+      question="Ещё не зарегистрированы?"
+      pathname="/signup"
+      textLink="Регистрация"
+      inputRef={firstInputRef}
+    />
+  );
+};
+
+export default Login;
