8ff01d5fe126229862c0991681f08ead10d9b436
Oleg Zharov <levineye@gmail.com>
2021-03-01 22:08:47 +0300

    feat: добавить адаптив для 404

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -7,11 +7,13 @@ const NotFound = () => {
 
   return (
     <section className="not-found">
-      <h2 className="not-found__title">404</h2>
-      <p className="not-found__subtitle">Страница не найдена</p>
-      <button type="button" className="not-found__back" onClick={goBack}>
-        Назад
-      </button>
+      <div className="not-found__content">
+        <h2 className="not-found__title">404</h2>
+        <p className="not-found__subtitle">Страница не найдена</p>
+        <button type="button" className="not-found__back" onClick={goBack}>
+          Назад
+        </button>
+      </div>
     </section>
   );
 };
