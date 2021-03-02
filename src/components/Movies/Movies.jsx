6a16e7d280f9a2457e1593da6a414394b418c236
Oleg Zharov <levineye@gmail.com>
2021-03-02 18:57:33 +0300

    feat: определить внешнюю геометрию внутри секции со всеми фильмами

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import SearchForm from './../SearchForm/SearchForm';
-import MoviesCardList from './../MoviesCardList/MoviesCardList';
+import SearchForm from '../SearchForm/SearchForm';
+import MoviesCardList from '../MoviesCardList/MoviesCardList';
+import ButtonAddMovies from '../ButtonAddMovies/ButtonAddMovies';
 import './Movies.css';
-import { images } from './../../utils/utils';
+import { images } from '../../utils/utils';
 
 const Movies = () => {
   return (
     <section className="movies page__movies">
       <SearchForm />
-      <MoviesCardList isSaved={false} movies={images} />
+      <MoviesCardList isSaved={false} movieList={images} />
+      <ButtonAddMovies isVisible={true} />
     </section>
   );
 };
