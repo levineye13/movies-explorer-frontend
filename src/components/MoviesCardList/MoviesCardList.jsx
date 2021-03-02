dcb3a149a6362f1d59db296acaf6706824af598a
Oleg Zharov <levineye@gmail.com>
2021-03-02 16:02:21 +0300

    feat: добавить адаптив для сетки с фильмами

--- src/components/MoviesCardList/MoviesCardList.jsx
+++ src/components/MoviesCardList/MoviesCardList.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import MoviesCard from './../MoviesCard/MoviesCard';
 import './MoviesCardList.css';
-import { images } from './../../utils/utils';
 
-const MoviesCardList = () => {
+const MoviesCardList = ({ isSaved, movies }) => {
   return (
-    <ul className="movies-cardlist movies__movies-cardlist">
-      {images.map((image, index) => (
+    <ul className="movies-cardlist movies__movies-cardlist saved-movies__movies-cardlist">
+      {movies.map((movie, index) => (
         <li className="movies-cardlist__item" key={index}>
-          <MoviesCard img={image} />
+          <MoviesCard isSavedCard={isSaved} img={movie} />
         </li>
       ))}
     </ul>
