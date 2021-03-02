c3de51fb68cc9e68bca3398ff36102093a110786
Oleg Zharov <levineye@gmail.com>
2021-03-02 18:58:19 +0300

    feat: добавлять модификаторы в зависимости от пути

--- src/components/MoviesCardList/MoviesCardList.jsx
+++ src/components/MoviesCardList/MoviesCardList.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import MoviesCard from './../MoviesCard/MoviesCard';
 import './MoviesCardList.css';
+import { PATHNAME } from '../../utils/constants';
 
-const MoviesCardList = ({ isSaved, movies }) => {
+const { movies, saved } = PATHNAME;
+
+const MoviesCardList = ({ isSaved, movieList }) => {
+  const { pathname } = useLocation();
+
   return (
-    <ul className="movies-cardlist movies__movies-cardlist saved-movies__movies-cardlist">
-      {movies.map((movie, index) => (
-        <li className="movies-cardlist__item" key={index}>
-          <MoviesCard isSavedCard={isSaved} img={movie} />
-        </li>
-      ))}
+    <ul
+      className={`movies-cardlist ${
+        pathname === movies
+          ? 'movies__movies-cardlist'
+          : pathname === saved
+          ? 'saved-movies__movies-cardlist'
+          : ''
+      }`}
+    >
+      {movieList &&
+        movieList.map((movie, index) => (
+          <li className="movies-cardlist__item" key={index}>
+            <MoviesCard isSavedCard={isSaved} img={movie} />
+          </li>
+        ))}
     </ul>
   );
 };
