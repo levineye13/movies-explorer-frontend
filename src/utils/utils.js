ac1227d33d642c2d02d51e6d8b1b62ed4f721304
Oleg Zharov <levineye@gmail.com>
2021-03-08 19:00:33 +0300

    feat: написать функцию для получения нужных ключей из объекта movie

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,30 +1,4 @@
-import img1 from './../images/movies-cards/pic__COLOR_pic1.jpg';
-import img2 from './../images/movies-cards/pic__COLOR_pic2.jpg';
-import img3 from './../images/movies-cards/pic__COLOR_pic3.jpg';
-import img4 from './../images/movies-cards/pic__COLOR_pic4.jpg';
-import img5 from './../images/movies-cards/pic__COLOR_pic5.jpg';
-import img6 from './../images/movies-cards/pic__COLOR_pic6.jpg';
-import img7 from './../images/movies-cards/pic__COLOR_pic7.jpg';
-import img8 from './../images/movies-cards/pic__COLOR_pic8.jpg';
-import img9 from './../images/movies-cards/pic__COLOR_pic9.jpg';
-import img10 from './../images/movies-cards/pic__COLOR_pic10.jpg';
-import img11 from './../images/movies-cards/pic__COLOR_pic11.jpg';
-import img12 from './../images/movies-cards/pic__COLOR_pic12.jpg';
-
-export const images = [
-  img1,
-  img2,
-  img3,
-  img4,
-  img5,
-  img6,
-  img7,
-  img8,
-  img9,
-  img10,
-  img11,
-  img12,
-];
+'use strict';
 
 export const checkDisplayComponent = (
   allowPathList,
@@ -34,4 +8,22 @@ export const checkDisplayComponent = (
   if (allowPathList.includes(currentPath)) {
     return Component;
   }
+};
+
+export const createObjectFromKeys = (obj, keysList) => {
+  return keysList.reduce((acc, key) => {
+    if (key.includes('.')) {
+      key = key.split('.');
+      const newKey = key[key.length - 1];
+      const newValue = key.reduce((prev, path) => {
+        if (prev === undefined || prev === null) {
+          return undefined;
+        }
+        acc = { ...acc, [newKey]: prev[path] };
+        return prev[path];
+      }, obj);
+    }
+
+    return obj.hasOwnProperty(key) ? { ...acc, [key]: obj[key] } : acc;
+  }, {});
 };
