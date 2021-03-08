46b118cb86369dc8bf303729a4cc7cc1e642e71a
Oleg Zharov <levineye@gmail.com>
2021-03-08 18:56:26 +0300

    feat: передавать в компонент формы поиска коллбэк сабмита

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -2,13 +2,19 @@ import React from 'react';
 import FilterCheckbox from './../FilterCheckbox/FilterCheckbox';
 import './SearchForm.css';
 
-const SearchForm = () => {
+const SearchForm = ({ onSubmit }) => {
+  const handleSubmit = async (evt) => {
+    evt.preventDefault();
+    await onSubmit();
+  };
+
   return (
     <form
       action="#"
       className="search-form movies__search-form saved-movies__search-form"
       name="searchForm"
       noValidate
+      onSubmit={handleSubmit}
     >
       <label className="search-form__label">
         <span className="search-form__search-img" />
