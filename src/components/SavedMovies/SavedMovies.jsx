0f95debc070d2ea1fca39f238bf05ffb9fed8dc3
Oleg Zharov <levineye@gmail.com>
2021-03-08 18:59:53 +0300

    feat: удалить картинки загшки для верстки

--- src/components/SavedMovies/SavedMovies.jsx
+++ src/components/SavedMovies/SavedMovies.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 import SearchForm from './../SearchForm/SearchForm';
 import MoviesCardList from './../MoviesCardList/MoviesCardList';
 import './SavedMovies.css';
-import { images } from './../../utils/utils';
 
 const SavedMovies = () => {
   return (
     <section className="saved-movies page__saved-movies">
       <SearchForm />
-      <MoviesCardList isSaved={true} movieList={images.slice(0, 3)} />
+      <MoviesCardList isSaved={true} movieList={[]} />
     </section>
   );
 };
