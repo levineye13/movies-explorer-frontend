fa771ac6e8b28a3e8a3c32ed4025ceff0e196445
Oleg Zharov <levineye@gmail.com>
2021-03-08 18:50:35 +0300

    feat: сделать карточку ссылкой

--- src/components/MoviesCard/MoviesCard.jsx
+++ src/components/MoviesCard/MoviesCard.jsx
@@ -3,9 +3,10 @@ import './MoviesCard.css';
 
 const MoviesCard = ({
   isSavedCard = false,
-  title = 'В погоне за Бенкси',
-  time = '27 минут',
-  img,
+  title = '',
+  time = '',
+  img = '',
+  trailerLink = '',
 }) => {
   const [isSaved, setIsSaved] = useState(isSavedCard);
 
@@ -15,30 +16,36 @@ const MoviesCard = ({
 
   return (
     <article className="card">
-      <figure className="card__info">
-        <figcaption className="card__description">
-          <h2
-            className={`card__title ${isSavedCard ? 'card__title_saved' : ''}`}
-          >
-            {title}
-          </h2>
-          <p className={`card__time ${isSavedCard ? 'card__time_saved' : ''}`}>
-            {time}
-          </p>
-        </figcaption>
-        <img src={img} alt={`Фильм: "${title}"`} className="card__img" />
-      </figure>
-      <button
-        className={`card__save ${
-          isSavedCard
-            ? 'card__save_type_delete'
-            : isSaved
-            ? 'card__save_type_saved'
-            : ''
-        }`}
-        type="button"
-        onClick={toggleStateCard}
-      />
+      <a href={trailerLink} className="card__overlay" target="_blank">
+        <figure className="card__info">
+          <figcaption className="card__description">
+            <h2
+              className={`card__title ${
+                isSavedCard ? 'card__title_saved' : ''
+              }`}
+            >
+              {title}
+            </h2>
+            <p
+              className={`card__time ${isSavedCard ? 'card__time_saved' : ''}`}
+            >
+              {time}
+            </p>
+          </figcaption>
+          <img src={img} alt={`Фильм: "${title}"`} className="card__img" />
+        </figure>
+        <button
+          className={`card__save ${
+            isSavedCard
+              ? 'card__save_type_delete'
+              : isSaved
+              ? 'card__save_type_saved'
+              : ''
+          }`}
+          type="button"
+          onClick={toggleStateCard}
+        />
+      </a>
     </article>
   );
 };
