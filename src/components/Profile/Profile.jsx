88af679c2ce198badce005c1eb54552e65f20b23
Oleg Zharov <levineye@gmail.com>
2021-03-09 14:16:25 +0300

    feat: подключить валидацию для профайла

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import './Profile.css';
+import { useFormWithValidation } from '../../hooks/useFormWithValidation';
 
 const Profile = () => {
+  const { values, isValidForm, handleInputChange } = useFormWithValidation();
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+  };
+  console.log(isValidForm);
   return (
     <section className="profile page__profile">
       <h1 className="profile__title">Привет, Олег!</h1>
-      <form action="#" className="profile__form" name="profileForm" noValidate>
+      <form
+        action="#"
+        className="profile__form"
+        name="profileForm"
+        noValidate
+        onSubmit={handleSubmit}
+      >
         <fieldset className="profile__user-info">
           <label className="profile__field">
             Имя
@@ -14,6 +27,8 @@ const Profile = () => {
               className="profile__input"
               name="nameInput"
               required
+              onChange={handleInputChange}
+              value={values.nameInput || ''}
             />
           </label>
           <label className="profile__field">
@@ -23,10 +38,12 @@ const Profile = () => {
               className="profile__input"
               name="emailInput"
               required
+              onChange={handleInputChange}
+              value={values.emailInput || ''}
             />
           </label>
         </fieldset>
-        <button className="profile__edit" type="submit">
+        <button className="profile__edit" type="submit" disabled={!isValidForm}>
           Редактировать
         </button>
         <button className="profile__logout" type="submit">
