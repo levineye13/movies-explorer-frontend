8d9f3a04ba34f872b99601ac4dcc8066e0c2930e
Oleg Zharov <levineye@gmail.com>
2021-03-13 14:18:31 +0300

    feat: добавить логику в блок сохраненных фильмов

--- src/components/SavedMovies/SavedMovies.jsx
+++ src/components/SavedMovies/SavedMovies.jsx
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import SearchForm from './../SearchForm/SearchForm';
 import MoviesCardList from './../MoviesCardList/MoviesCardList';
+import Preloader from '../Preloader/Preloader';
 import './SavedMovies.css';
+import { filterShortMovies } from '../../utils/utils';
 
-const SavedMovies = () => {
+const SavedMovies = ({ savedMovies, onSubmit, onDeleteMovie }) => {
+  const [shortMovies, setShortMovies] = useState([]);
+  const [isShorted, setIsShorted] = useState(false);
+  const [isActivePreloader, setIsActivePreloader] = useState(false);
+
+  const handleSubmit = async (keyword) => {
+    setIsActivePreloader(true);
+    await onSubmit(keyword);
+    setIsActivePreloader(false);
+  };
+
+  const toggleShortMovies = () => {
+    setIsShorted(!isShorted);
+  };
+
+  useEffect(() => {
+    setShortMovies(filterShortMovies(shortMovies));
+  }, [shortMovies]);
+
   return (
     <section className="saved-movies page__saved-movies">
-      <SearchForm />
-      <MoviesCardList isSaved={true} movieList={[]} />
+      <Preloader isActive={isActivePreloader} />
+      <SearchForm onSubmit={handleSubmit} filter={toggleShortMovies} />
+      <MoviesCardList
+        isSaved={true}
+        movieList={!isShorted ? savedMovies : savedMovies}
+        onDeleteMovie={onDeleteMovie}
+      />
     </section>
   );
 };
