cc9219f49f3cd301f10b69575336d57b1585c035
Oleg Zharov <levineye@gmail.com>
2021-03-15 19:50:23 +0300

    feat: JSDOC

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -4,6 +4,14 @@ import { DISPLAY_RESOLUTION } from './constants';
 
 const { smallScreen, mediumScreen } = DISPLAY_RESOLUTION;
 
+/**
+ * Функция возвращает компонент для отображения, если текущий
+ * location входит в пул доступных адресов из списка.
+ *
+ * @param  {Array} allowPathList
+ * @param  {String} currentPath
+ * @param  {React.Component} Component
+ */
 export const checkDisplayComponent = (
   allowPathList,
   currentPath,
@@ -14,6 +22,18 @@ export const checkDisplayComponent = (
   }
 };
 
+/**
+ * Проверяет наличие ключей в объекте.
+ * const obj = {
+ *   image: {
+ *     url: 'address'
+ *   }
+ * }
+ * Вызов checkObjectProperty(obj, 'image.url') выведет строку 'address'.
+ *
+ * @param  {Object} obj
+ * @param  {String} path
+ */
 export const checkObjectProperty = (obj, path) => {
   const keys = path.split('.');
   return keys.reduce((prev, key) => {
@@ -24,8 +44,17 @@ export const checkObjectProperty = (obj, path) => {
   }, obj);
 };
 
+/**
+ * Возвращает ширину экрана.
+ */
 export const getScreenWidth = () => window.screen.width;
 
+/**
+ * Возвращает базовое и добавляемое количество карточек
+ * для разных разрешений экрана устройства.
+ *
+ * @param  {Number} screenWidth
+ */
 export const determineNumberOfCards = (screenWidth) => {
   switch (true) {
     case screenWidth > mediumScreen:
@@ -48,6 +77,11 @@ export const determineNumberOfCards = (screenWidth) => {
   }
 };
 
+/**
+ * Форматирование времени для отображения на компоненте карточки.
+ *
+ * @param  {Number} minutes
+ */
 export const setTimeFormat = (minutes) => {
   const hours = (minutes / 60).toString()[0];
   minutes %= 60;
@@ -55,6 +89,14 @@ export const setTimeFormat = (minutes) => {
   return +hours === 0 ? `${minutes} минут` : `${hours}ч ${minutes}мин`;
 };
 
+/**
+ * Функция проверяет, содержат ли заданные свойства объектов массива ключевое слово.
+ * При этом регистр и пробелы по краям не учитываются.
+ *
+ * @param  {Array} collection
+ * @param  {Array} propertyList
+ * @param  {String} keyword
+ */
 export const filterByKeyword = (collection, propertyList, keyword) =>
   collection.filter((item) =>
     propertyList.find((property) => {
@@ -70,5 +112,10 @@ export const filterByKeyword = (collection, propertyList, keyword) =>
     })
   );
 
+/**
+ * Функция определения короткометражек.
+ *
+ * @param  {Array} collection
+ */
 export const filterShortMovies = (collection) =>
   collection.filter((item) => item.duration <= 40);
