777b8b87a618add3392d18bf54bf88e9cea61b9a
Oleg Zharov <levineye@gmail.com>
2021-03-15 18:16:32 +0300

    refactor: удалить неиспользуемый код

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -14,24 +14,6 @@ export const checkDisplayComponent = (
   }
 };
 
-export const createObjectFromKeys = (obj, keysList) => {
-  return keysList.reduce((acc, key) => {
-    if (key.includes('.')) {
-      key = key.split('.');
-      const newKey = key[key.length - 1];
-      const newValue = key.reduce((prev, path) => {
-        if (prev === undefined || prev === null) {
-          return undefined;
-        }
-        acc = { ...acc, [newKey]: prev[path] };
-        return prev[path];
-      }, obj);
-    }
-
-    return obj.hasOwnProperty(key) ? { ...acc, [key]: obj[key] } : acc;
-  }, {});
-};
-
 export const checkObjectProperty = (obj, path) => {
   const keys = path.split('.');
   return keys.reduce((prev, key) => {
